8f0fbb3377c490afee9ff09dd3463da8d7030de4
Sarah Burtenshaw <sarahburtenshaw@Sarahs-MacBook-Air.local>
2023-05-16 14:33:18 -0600

    completed crud and edited seed to add one more item (left_over)

--- config.sh
+++ config.sh
@@ -1,1 +1,2 @@
 export POSTGRES_URI="postgresql://sarahburtenshaw:\$coobydoO2@localhost:5432/budget"
+env
---
+++ crud.py
@@ -0,0 +1,32 @@
+from model import db, User, Budget, connect_to_db
+import os 
+
+def create_user(email, password):
+    
+    user = User(email=email, password=password)
+    
+    return user
+
+def create_budget(income, bills, other, account_history, left_over):
+    
+    budget = Budget(
+        income=income,
+        bills=bills, 
+        other=other,
+        account_history=account_history,
+        left_over=left_over
+    )
+    return budget
+
+def get_users():
+    return User.query.all()
+
+def get_user_by_id(user_id):
+    return User.query.get(user_id)
+
+def get_user_by_email(email):
+    return User.query.filter(User.email == email).first()
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app, db_uri=os.environ['POSTGRES_URI'])
