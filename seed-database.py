9ce8aec46e037e63d262430ccf287456dd88951e
Sarah Burtenshaw <sarahburtenshaw@Sarahs-MacBook-Air.local>
2023-05-16 14:03:36 -0600

    completed server and seed

---
+++ seed-database.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+import json
+import os
+import crud
+import model
+import server
+
+os.system('dropdb budget')
+os.system('createdb budget')
+
+model.connect_to_db(server.app, db_uri=os.environ['POSTGRES_URI'])
+model.db.create_all()
+
+with open('data/movies.json') as f:
+    budget_data = json.loads(f.read())
+
+budget_in_db = []
+
+for budget in budget_data:
+    income, bills, other, account_history = (
+        budget["income"],
+        budget["bills"],
+        budget["other"],
+        budget["account_history"]
+    )
+    last_updated = datetime.strptime(budget["account_changed"], "%Y-%m-%d")
+    
+    db_budget = crud.create_budget(income, bills, other, account_history, last_updated)
+    budget_in_db.append(db_budget)
+model.db.session.add_all(budget_in_db)
+model.db.session.commit()    
--- server.py
+++ server.py
@@ -1,14 +1,70 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, flash, session, redirect
 from flask_cors import CORS
+import crud
+from model import connect_to_db, db
+import os
 
 app = Flask(__name__)
 cors = CORS(app)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def homepage():
+    return render_template('homepage.html')
 
+@app.route("/create-account")
+def create_account():
+    
+    create = crud.get_account()
+    
+    return render_template("create-page.html", create=create)
+
+@app.route("/users", methods=["POST"])
+def register_user():
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    user = crud.get_user_by_email(email)
+    if user:
+        flash("Can't make an account with this email. Please try again.")
+    else:
+        user = crud.create_user(email, password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Account has been sucessfully created")
+    return redirect("/")
+
+@app.route("/budget")
+def budget():
+    
+    budget_list = crud.get_budget()
+    
+    return render_template("/budget.html", budget_list=budget_list)
+
+@app.route("/budget", methods=["POST"])
+def get_budget():
+    
+    bills = request.json["bills_id"]
+    other = request.json["other_id"]
+    account_history = request.json["account-history"]
+    crud.budget_update(bills, other, account_history)
+    db.session.commit()
+    
+@app.route("/login", methods=["POST"])
+def process_login():
+    
+    email = request.form.get("email")
+    password = request.form.get("password")
+    
+    user = crud.get_user_by_email(email)
+    if not user or user.password != password:
+        flash("The email or password you entered was incorrect")
+    else:
+        session["user_email"] = user.email
+        flash(f"Successfully Logged in")
+    return redirect("/")    
 
 
 if __name__ == "__main__":
-    app.run()
+    connect_to_db(app, db_uri=os.environ['POSTGRES_URI'])
+    app.run(debug = True, port = 6287, host = "localhost")
